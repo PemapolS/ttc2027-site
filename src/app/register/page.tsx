5251de104872f9ff3c74bf64991c4d38ed2e8c77
PemapolS <138964662+PemapolS@users.noreply.github.com>
2026-03-27 15:45:55 +0700

    use client fix

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,8 @@
+'use client';
+
 import ComingSoon from '@/components/ComingSoon';
 import PageLayout from '@/components/PageLayout';
 import { useI18n } from '@/lib/i18n';
-import Link from 'next/link';
 
 export default function RegisterPage() {
   const { t } = useI18n();
